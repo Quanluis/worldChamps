a04ddfc2c27f5ecec025cc39f6b9dbb3bbd78d58
Quanluis <quanluis@protonmail.com>
2020-11-03 12:17:26 -0500

    Finished program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.calculate = new System.Windows.Forms.Button();
             this.results = new System.Windows.Forms.TextBox();
             this.winningsLabel = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // exitButton
@@ -110,6 +111,7 @@
             this.calculate.TabIndex = 4;
             this.calculate.Text = "Calculate";
             this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
             // 
             // results
             // 
@@ -128,11 +130,22 @@
             this.winningsLabel.TabIndex = 6;
             this.winningsLabel.Text = "World Championships won :";
             // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(461, 372);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
             this.Controls.Add(this.winningsLabel);
             this.Controls.Add(this.results);
             this.Controls.Add(this.calculate);
@@ -141,7 +154,7 @@
             this.Controls.Add(this.Headline);
             this.Controls.Add(this.exitButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "World Champs";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +169,7 @@
         private System.Windows.Forms.Button calculate;
         private System.Windows.Forms.TextBox results;
         private System.Windows.Forms.Label winningsLabel;
+        private System.Windows.Forms.Button Clear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.IsolatedStorage;
 
 namespace worldChamps
 {
     public partial class Form1 : Form
+
     {
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void exitButton_Click(object sender, EventArgs e)
+		// World series winners array
+
+	string[] seriesWinners = 
+	{ 
+	"Boston Americans","New York Giants","Chicago White Sox",
+	"Chicago Cubs","Chicago Cubs","Pittsburgh Pirates","Philadelphia Athletics",
+	"Philadelphia Athletics","Boston Red Sox","Philadelphia Athletics","Boston Braves","Boston Red Sox",
+	"Boston Red Sox","Chicago White Sox","Boston Red Sox","Cincinnati Reds","Cleveland Indians",
+	"New York Giants","New York Giants","New York Yankees","Washington Senators","Pittsburgh Pirates",
+	"St.Louis Cardinals","New York Yankees","New York Yankees","Philadelphia Athletics",
+	"Philadelphia Athletics","St.Louis Cardinals","New York Yankees","New York Giants",
+	"St.Louis Cardinals","Detroit Tigers","New York Yankees","New York Yankees",
+	"New York Yankees","New York Yankees","Cincinnati Reds","New York Yankees",
+	"St.Louis Cardinals","New York Yankees","St.Louis Cardinals","Detroit Tigers",
+	"St.Louis Cardinals","New York Yankees","Cleveland Indians","New York Yankees",
+	"New York Yankees","New York Yankees","New York Yankees",
+	"New York Yankees","New York Giants","Brooklyn Dodgers","New York Yankees",
+	"Milwaukee Braves","New York Yankees","Los Angeles Dodgers","Pittsburgh Pirates",
+	"New York Yankees","New York Yankees","Los Angeles Dodgers",
+	"St. Louis Cardinals","Los Angeles Dodgers","Baltimore Orioles","St. Louis Cardinals",
+	"Detroit Tigers","New York Mets","Baltimore Orioles","Pittsburgh Pirates",
+	"Oakland Athletics","Oakland Athletics","Oakland Athletics","Cincinnati Reds",
+	"Cincinnati Reds","New York Yankees","New York Yankees","Pittsburgh Pirates",
+	"Philadelphia Phillies","Los Angeles Dodgers","St.Louis Cardinals",
+	"Baltimore Orioles","Detroit Tigers","Kansas City Royals","New York Mets",
+	"Minnesota Twins","Los Angeles Dodgers","Oakland Athletics","Cincinnati Reds",
+	"Minnesota Twins","Toronto Blue Jays","Toronto Blue Jays","Atlanta Braves",
+	"New York Yankees","Florida Marlins","New York Yankees","New York Yankees",
+	"New York Yankees","Arizona Diamondbacks","Anaheim Angels","Florida Marlins",
+	"Boston Red Sox","Chicago White Sox","St. Louis Cardinals",
+	"Boston Red Sox","Philadelphia Phillies",
+	};
+
+		private void calculate_Click(object sender, EventArgs e)
+        {
+
+				//initialize the count to 0
+				int wins = 0;
+
+				for (int i = 0; i < seriesWinners.Length; i++)
+				{
+					
+					if (teamOptions.SelectedItem.ToString() == seriesWinners[i])
+					{
+						wins++;
+					}
+				}
+				
+				results.Text = Convert.ToString(wins);
+			}
+
+		// Clear button
+
+		private void Clear_Click(object sender, EventArgs e)
+		{
+			results.Text = "";
+		}
+
+		// Exit button
+
+		private void exitButton_Click(object sender, EventArgs e)
         {
             Close();
         }
+
     }
 }
